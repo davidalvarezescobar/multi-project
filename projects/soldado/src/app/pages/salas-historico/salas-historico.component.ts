000ae4411c05b1291340f1b7e9c244e30b7850f8
@jalarcon011 <javier.alarcon@softtek.com>
2024-02-16 11:53:32 +0100

    añadida vista admin a salas-listado historico

--- projects/soldado/src/app/pages/salas-historico/salas-historico.component.ts
+++ projects/soldado/src/app/pages/salas-historico/salas-historico.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-salas-historico',
   templateUrl: './salas-historico.component.html',
   styleUrls: ['./salas-historico.component.scss']
 })
-export class SalasHistoricoComponent {
+export class SalasHistoricoComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  tipoUsuario = '';
+  constructor(private router: Router,
+    private loginSrv: LoginService) {}
+  ngOnInit(): void {
+    this.loginSrv.user$.subscribe(user => {
+      this.tipoUsuario = user
+    });
+  }
   rows: any = [
     {
       pabellon: 'Pabellon 1',
       sala: 'Sala 1',
+      nombre: 'Paco Ortuño',
       fecha: '01/01/2024',
       horaDesde: '10:41',
       horaHasta: '11:30'
@@ -20,6 +29,7 @@ export class SalasHistoricoComponent {
     {
       pabellon: 'Pabellon 2',
       sala: 'Sala 2',
+      nombre: 'Jesús Buendía',
       fecha: '01/01/2024',
       horaDesde: '10:41',
       horaHasta: '11:30'
@@ -27,7 +37,24 @@ export class SalasHistoricoComponent {
     {
       pabellon: 'Pabellon 3',
       sala: 'Sala 3',
-      fecha: '01/01/2024',
+      nombre: 'Sergio Recuenco',
+      fecha: '12/01/2024',
+      horaDesde: '10:41',
+      horaHasta: '11:30'
+    },
+    {
+      pabellon: 'Pabellon 3',
+      sala: 'Sala 4',
+      nombre: 'Sergio Recuenco',
+      fecha: '07/02/2024',
+      horaDesde: '10:41',
+      horaHasta: '11:30'
+    },
+    {
+      pabellon: 'Pabellon 4',
+      sala: 'Sala 3',
+      nombre: 'Sergio Izquierdo',
+      fecha: '01/02/2024',
       horaDesde: '10:41',
       horaHasta: '11:30'
     }
