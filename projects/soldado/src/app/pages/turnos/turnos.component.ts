7190553d2f1513410fb48b7e47e42331f12a0a2c
@jalarcon011 <javier.alarcon@softtek.com>
2024-02-20 18:39:48 +0100

    cambios pantalla turnos

--- projects/soldado/src/app/pages/turnos/turnos.component.ts
+++ projects/soldado/src/app/pages/turnos/turnos.component.ts
@@ -7,6 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurnosComponent implements OnInit {
 
+  rows: any = [
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 1',
+      lunes: 'Mañana',
+      martes: 'Tarde',
+      miercoles: 'Noche',
+      jueves: 'Mañana',
+      viernes: 'Libre',
+      sabado: 'Mañana',
+      domingo: 'Tarde'
+    },
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 2',
+      lunes: 'Mañana',
+      martes: 'Noche',
+      miercoles: 'Mañana',
+      jueves: 'Tarde',
+      viernes: 'Libre',
+      sabado: 'Tarde',
+      domingo: 'Mañana'
+    },
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 3',
+      lunes: 'Mañana',
+      martes: 'Tarde',
+      miercoles: 'Noche',
+      jueves: 'Mañana',
+      viernes: 'Noche',
+      sabado: 'Tarde',
+      domingo: 'Libre'
+    },
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 4',
+      lunes: 'Mañana',
+      martes: 'Libre',
+      miercoles: 'Noche',
+      jueves: 'Noche',
+      viernes: 'Mañana',
+      sabado: 'Tarde',
+      domingo: 'Mañana'
+    },
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 5',
+      lunes: 'Mañana',
+      martes: 'Libre',
+      miercoles: 'Mañana',
+      jueves: 'Mañana',
+      viernes: 'Libre',
+      sabado: 'Noche',
+      domingo: 'Noche'
+    },
+    {
+      servicio: 'Patrulla Águila',
+      semana: 'Semana 6',
+      lunes: 'Mañana',
+      martes: 'Tarde',
+      miercoles: 'Noche',
+      jueves: 'Tarde',
+      viernes: 'Mañana',
+      sabado: 'Libre',
+      domingo: 'Mañana'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
