89898eaee086a788c6b6d96b4d0fbb146d93e296
SOFTTEKPROJECTS\david.alvarez <david.alvarez@softtek.com>
2024-02-26 17:54:43 +0100

    cambios excel tareas pendientes

--- projects/soldado/src/app/pages/comedor-historico-admin/comedor-historico-admin.component.ts
+++ projects/soldado/src/app/pages/comedor-historico-admin/comedor-historico-admin.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ComedorHistoricoAdminComponent {
   peticionesList = [
-    { comedor: "Principal", fecha: "2024-02-19", principal: "Croquetas", segundo: "Pasta carbonara", postre: "Flan casero", intorelancias: [], precioMenu: 12, pagado: true, recibido: true, entregado: true },
-    { comedor: "Cantina", fecha: "2024-02-19", principal: "Paella de marisco", segundo: "Pollo a la parrilla", postre: "Helado", intorelancias: [], precioMenu: 12, pagado: false, recibido: true, entregado: false },
-    { comedor: "Edificio 2", fecha: "2024-02-20", principal: "Alubias a la riojana", segundo: "Salmón al horno", postre: "Fruta", intorelancias: [], precioMenu: 12, pagado: true, recibido: false, entregado: true }
+    { comedor: "Principal", peticionario: 'Teniente Dam', fecha: "2024-02-19", principal: "Croquetas", segundo: "Pasta carbonara", postre: "Flan casero", intorelancias: [], precioMenu: 12, pagado: true, recibido: true, entregado: true },
+    { comedor: "Cantina", peticionario: 'Capitán Lucas', fecha: "2024-02-19", principal: "Paella de marisco", segundo: "Pollo a la parrilla", postre: "Helado", intorelancias: [], precioMenu: 12, pagado: false, recibido: true, entregado: false },
+    { comedor: "Edificio 2", peticionario: 'Sargento Gil',  fecha: "2024-02-20", principal: "Alubias a la riojana", segundo: "Salmón al horno", postre: "Fruta", intorelancias: [], precioMenu: 12, pagado: true, recibido: false, entregado: true }
   ];
 
 }
